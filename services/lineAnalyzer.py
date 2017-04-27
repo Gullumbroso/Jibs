0bf4e19e3fe36b5e52be91b84fb63a9de8f56999
omer_alon <omeralon3@gmail.com>
2017-04-27 19:58:12 +0300

    REFRESH

--- services/lineAnalyzer.py
+++ services/lineAnalyzer.py
@@ -1,1 +1,28 @@
+import services.dateAnalyzer as da
 
+
+
+
+def line_anayzer(line):
+
+
+
+    da.time_analyzer()
+
+
+
+# Imports the Google Cloud client library
+from google.cloud import language
+
+# Instantiates a client
+language_client = language.Client()
+
+# The text to analyze
+text = 'Hello, world!'
+document = language_client.document_from_text(text)
+
+# Detects the sentiment of the text
+sentiment = document.analyze_sentiment().sentiment
+
+print('Text: {}'.format(text))
+print('Sentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))

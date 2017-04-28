3197160790c258135dfa06fbc2f33b1c815897d9
Gilad Lumbroso <gullumbroso@gmail.com>
2017-04-28 06:40:23 +0300

    Django!

--- jibsapp/views.py
+++ jibsapp/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+import services as services
 
-# Create your views here.
+class WhatToDo(APIView):
+    def get(self, request):
+        # Prepare the graph
+        params = request.query_params
+        if len(params) != 1:
+            return Response("Please enter a url to parse.", status=status.HTTP_204_NO_CONTENT)
+        else:
+            first_answer = params['firstAnswer']
+            article_title, article_content = services.parse_article(url)
+            whole_content = article_title + '\n' + article_content
+            prediction, score = services.
+            response = {
+                'article_title': article_title,
+                'article_content': article_content,
+                'prediction': prediction,
+                'confidence': score
+            }
+
+            return Response(response, status=status.HTTP_200_OK)

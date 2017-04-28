44fe751dbae274c1a161c9a2221cc4123915e6b3
omer_alon <omeralon3@gmail.com>
2017-04-28 11:55:06 +0300

    6 minute

--- jibsapp/views.py
+++ jibsapp/views.py
@@ -5,6 +5,8 @@ from rest_framework.response import Response
 import services as services
 from services.lineAnalyzer import *
 
+session = None
+
 class FirstAnswer(APIView):
     def get(self, request):
         # Prepare the graph
@@ -18,6 +20,31 @@ class FirstAnswer(APIView):
             response = {
                 'type': session.stype,
                 'data': session.data_I_know(),
+                'next': session.whats_next(),
+                'is_done': session.is_mandatory_done()
+            }
+
+            return Response(response, status=status.HTTP_200_OK)
+
+
+class updateData(APIView):
+    def get(self, request):
+        # Prepare the graph
+        params = request.query_params
+        if len(params) != 1:
+            return Response("Please enter a url to parse.", status=status.HTTP_204_NO_CONTENT)
+        else:
+            if params['type'] == "content":
+                session.add_to_subject(params['answer']['subject'])
+                session.add_to_body(params['answer']['body'])
+
+            if params['type'] == "people":
+                session.add_person(params['answer'])
+
+            response = {
+                'type': session.stype,
+                'data': session.data_I_know(),
+                'next': session.whats_next(),
                 'is_done': session.is_mandatory_done()
             }
 
--- services/sessions/Event.py
+++ services/sessions/Event.py
@@ -21,3 +21,50 @@ class Event(Session):
     def set_color(self, color):
         self.color = color
 
+    def has_time(self):
+        return self.time is not None
+
+    def has_title(self):
+        return bool(self.title)
+
+    def has_place(self):
+        return bool(self.place)
+
+    def has_color(self):
+        return self.color is not None
+
+    def whats_next(self):
+        if not self.has_time():
+            return "time"
+
+        if not self.has_title():
+            return "title"
+
+        if not self.has_place():
+            return "place"
+
+        if not self.has_color():
+            return "color"
+
+        if not self.has_persons():
+            return "persons"
+
+        return None
+
+    def is_mandatory_done(self):
+        return self.has_time() and self.has_title()
+
+    def data_I_know(self):
+        data = {}
+        if self.persons:
+            data["persons"] = self.persons
+        if self.title:
+            data["title"] = self.title
+        if self.time:
+            data["time"] = self.time
+        if self.place:
+            data["place"] = self.place
+        if self.color:
+            data["color"] = self.color
+
+        return data
